108ed3d651f9580dc38817ddcc69cb49fefe0081
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2021-01-25 04:04:47 -0300

    refactor: add comments

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,12 @@ import (
 	"os"
 )
 
-// write object pointer
+// writer object pointer
 var writer *bufio.Writer
 
 func main() {
+	// All output will be redirected to
+	// bufio.NewWriter
 	writer = bufio.NewWriter(os.Stdout)
 	s := "How many stars does Orion have?\n"
 	var b byte = 'H'
@@ -17,5 +19,6 @@ func main() {
 	writer.WriteByte(b)
 	writer.WriteString("\n")
 
+	// close buffer
 	defer writer.Flush()
 }
